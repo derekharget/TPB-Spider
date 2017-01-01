609cebfaa331daf617f2eb8e2df378213c1aae37
Derek Harget <git@derekharget.com>
2017-01-01 08:12:12 -0500

    Added config.php
    Added in config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define(M_HOST, "127.0.0.1"); //IP or HOST of DB Server
+define(M_PORT, "3306"); //Port of DB Server
+define(M_USER, "USER"); //User of DB Server
+define(M_PASS, "PASS"); //Pass of DB Server
+define(M_DATA, "database"); //Database to select
+
+?>
